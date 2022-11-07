7c863274a97314e280c9725ec2478e9dd59f90ec
sorasora313 <sky.313.sora@gmail.com>
2022-11-08 06:25:38 +0900

    Create ruby74.rb

---
+++ ruby74.rb
@@ -0,0 +1,16 @@
+def bubble_sort(data)
+  length = data.length 
+  for i in 0..(length-1) 
+    for j in 1.. (length-i-1) 
+      if data[j-1] < data[j] 
+        data[j-1],data[j] = data[j],data[j-1] 
+      end
+    end
+  end
+end
+
+number = [1,23,4,6,12,45,79]
+
+bubble_sort(number)
+
+puts number
